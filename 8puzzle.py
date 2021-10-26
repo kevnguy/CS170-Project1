19730643c2c692e18589db2e252085daeae6be96
kevnguy <56405177+kevnguy@users.noreply.github.com>
2021-10-26 15:15:41 -0700

    Add general search

--- 8puzzle.py
+++ 8puzzle.py
@@ -1,17 +1,56 @@
+import copy
+from collections import deque
+
 goal = [[1,2,3],
         [4,5,6],
-        [7,8,0]]     
+        [7,8,0]]
+
+seen = {}
         
 class Node:
-    def __init__(self, puzzle, cost, depth) -> None:
-        self.puzzle = puzzle
-        self.cost = cost
+    def __init__(self, board, depth, parent=None) -> None:
+        self.board = board
         self.depth = depth
-        self.children = []
-        self.parent
+        self.parent = parent
+        if(self.parent != None):
+            self.gcost = parent.gcost + 1
+        else:
+            self.gcost = 0
+
+def printPuzzle(puzzle) -> None:
+    for row in puzzle:
+        print(*row, sep=' ')
+
+def search(root, heristic) -> Node:
+    """
+    General search algorithm to find a solution if it exists
+    """
+    expansions = 0
+    queue = deque([root])
+    seen.add(root)
+
+    while queue:
+        front_node = queue.popleft()
+        if(front_node.board == goal):
+            print("Nodes expanded: {}", expansions)
+            return front_node
+        else:
+            # update queue with queuing function
+            queue = QueueFunction(front_node, queue, heristic)
+            expansions += 1
+    else:
+        print("No solution found") 
+
+#TODO
+def QueueFunction(node, queue, heristic):
+    pass
+    """
+    Queuing function used to expand node and update queue
+    """
+
 
 def main():
-    print(goal)
+    printPuzzle(goal)
 
 if __name__ == "__main__":
     main()
