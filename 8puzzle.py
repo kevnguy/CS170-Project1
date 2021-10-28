99c5e5ada037ab727f7fc40b6b82af4fde32b1cc
kevnguy <56405177+kevnguy@users.noreply.github.com>
2021-10-28 00:34:46 -0700

    Work on menu

--- 8puzzle.py
+++ 8puzzle.py
@@ -252,6 +252,38 @@ class Evaluater:
             return self.manhattanDistanceHeuristic(node)
         return 0
 
+def menu():
+    print("Welcome to my scuffed program")
+    chooseInput = ''
+    heuristicChoice = 0
+    inputPuzzle = []
+    while True:
+        chooseInput = input("Enter 0 to use default puzzle or 1 to input a custom puzzle\n")
+        if chooseInput == '0' or chooseInput == '1': 
+            break
+        else: 
+            continue
+
+    if chooseInput == '0':
+        inputPuzzle = [[8,6,7],
+                       [2,5,4],
+                       [3,0,1]]
+    else:
+        row = int(input("Please enter the number of rows in the puzzle: "))
+        col = int(input("Please enter the number of columns in the puzzle: "))
+        print("Please enter the rows of your puzzle with 0 representing the blankspace. " +
+               "Enter only number, delimit the values with a space, and press enter after each row")
+        for i in range(row):
+            inputPuzzle.append([int(item) for item in input("Enter {}st row: ".format(i+1)).split()])
+
+    print("\nNext, please choose the desired search heuristic")
+    print("  (1) A* with no heuristic (Uniform Cost Search)")
+    print("  (2) A* with Misplaced Tile Heuristic")
+    print("  (3) A* with Manhattan Distance Heuristic\n")
+    heuristicChoice = int(input())-1
+
+    return inputPuzzle, heuristicChoice
+
 def main():
     """
     Main function
